e81b24ab0dbf2e27e6a412aa703c9d2638893984
suvadip717 <suvadipdas717@gmail.com>
2024-02-06 23:47:34 +0530

    fix all register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async(localFilePath) => {
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         //File has been uploaded successfull
-        console.log("File is uploaded on cloudinary ", response.url);
+        // console.log("File is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
         
     } catch (error) {
